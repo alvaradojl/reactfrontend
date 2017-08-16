f26dea5d5fe9f680645235bf735f26bfc2cddad9
jorge.alvarado@elca.ch <jorge.alvarado@elca.ch>
2017-08-16 21:20:14 +0200

    installed web3 and instantiated from metamask

--- src/app/components/Web3/Web3Form.js
+++ src/app/components/Web3/Web3Form.js
@@ -4,15 +4,37 @@ import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
 import styleSheet from './styleSheet';
 import PropTypes from 'prop-types';
+import Web3 from 'web3';
 
 class Web3Form extends React.Component{
     constructor(props){
         super(props); 
     }
 
+    componentDidMount() {
+        window.addEventListener('load', function () { 
+          
+            let web3Proxy = new Web3();
+            web3Proxy.setProvider(web3.currentProvider);
+           
+            var coinbase = web3Proxy.eth.coinbase;
+            var originalBalance = web3Proxy.eth.getBalance(coinbase,function(error,result){
+                console.log("balance found: " + result);
+            });
+
+            web3Proxy.net.getPeerCount(function(error, result){ 
+                console.log("peers found: " + result);
+             });
+            
+           
+        });
+      }
+
     render(){
 
         const { classes } = this.props;
+
+        
 
         return(
             <Grid container className={classes.container}>
@@ -24,7 +46,7 @@ class Web3Form extends React.Component{
                     Web3 Features
                 </Typography>
 
-                <span>{web3.eth.defaultAccount}</span>
+              
  
                     <Grid container className={classes.root}>
                         <Grid item md={12}>
